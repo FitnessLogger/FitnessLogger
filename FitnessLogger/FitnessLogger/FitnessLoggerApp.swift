36140c10ad6c98a6bfc87f71670d1dc58650ed02
Jaafar Mahdi <als-92@hotmail.com>
2020-09-28 09:38:07 +0200

    Initial commit

---
+++ FitnessLogger/FitnessLogger/FitnessLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessLoggerApp.swift
+//  FitnessLogger
+//
+//  Created by Jaafar Mahdi on 28/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct FitnessLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
