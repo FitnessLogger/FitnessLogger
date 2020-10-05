917b287eec8e7fd0bde5347ef15a50f0fc655cef
Jaafar Mahdi <als-92@hotmail.com>
2020-10-05 08:50:29 +0200

    Navigate to sheet on list view click

--- FitnessLogger/FitnessLogger/Views/ProgramDetailView.swift
+++ FitnessLogger/FitnessLogger/Views/ProgramDetailView.swift
@@ -3,11 +3,18 @@ import SwiftUI
 
 struct ProgramDetailView: View {
     var trainingProgram: TrainingProgram
+    @State private var showSheet: Bool = false
     
     var body: some View {
         VStack {
             List(trainingProgram.exercises) { exercise in
+                Button("", action: {
+                    print(exercise.name)
+                    self.showSheet.toggle()
+                })
                 ExerciseDetailItem(exercise: exercise)
+            }.sheet(isPresented: self.$showSheet) {
+                Text("Hello world")
             }
         }
         .navigationBarTitle(Text(self.trainingProgram.name))
