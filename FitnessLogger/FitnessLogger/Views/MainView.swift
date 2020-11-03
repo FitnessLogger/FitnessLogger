06bc576f1d64ec6cb895027689b78cf5fd6ad357
Jaafar Mahdi <als-92@hotmail.com>
2020-11-03 13:21:00 +0100

    Merge branch 'develop' into login-screen

--- FitnessLogger/FitnessLogger/Views/MainView.swift
+++ FitnessLogger/FitnessLogger/Views/MainView.swift
@@ -12,37 +12,44 @@ struct MainView: View {
     }
 
     var body: some View {
-        Group {
-            if (session.session != nil) {
-                TabView {
-                    NavigationView {
-                        TrainingProgramView(tp: tp).navigationBarTitle(Text("12 - 15 reps"))
-                    }.tabItem {
-                        Image(systemName: "1.circle")
-                        Text("Easy")
-                    }.tag(0)
+        ZStack {
+            Group {
+                if (session.session != nil) {
+                    TabView {
+                        NavigationView {
+                            TrainingProgramView(tp: tp).navigationBarTitle(Text("12 - 15 reps"))
+                        }.tabItem {
+                            Image(systemName: "1.circle")
+                            Text("Easy")
+                        }.tag(0)
 
 
-                    NavigationView {
-                        TrainingProgramView(tp: tp).navigationBarTitle(Text("8 - 10 reps"))
-                    }.tabItem {
-                        Image(systemName: "2.circle")
-                        Text("Medium")
-                    }.tag(1)
+                        NavigationView {
+                            TrainingProgramView(tp: tp).navigationBarTitle(Text("8 - 10 reps"))
+                        }.tabItem {
+                            Image(systemName: "2.circle")
+                            Text("Medium")
+                        }.tag(1)
 
 
-                    NavigationView {
-                        TrainingProgramView(tp: tp).navigationBarTitle(Text("3 - 5 reps"))
-                    }.tabItem {
-                        Image(systemName: "3.circle")
-                        Text("Hard")
-                    }.tag(2)
+                        NavigationView {
+                            TrainingProgramView(tp: tp).navigationBarTitle(Text("3 - 5 reps"))
+                        }.tabItem {
+                            Image(systemName: "3.circle")
+                            Text("Hard")
+                        }.tag(2)
+                    }
+                } else {
+                    SignInView()
                 }
-            } else {
-                SignInView()
+            }.onAppear(perform: {
+                getUser()
+            })
+            
+            if global.isLoading {
+                BlurView(style: .light)
+                LoadingView()
             }
-        }.onAppear(perform: {
-            getUser()
-        })
+        }
     }
 }
