a4261f2155e63fe0437e0b4a96140a481aae7693
Marcus Christiansen <mac@trackman.com>
2020-11-30 17:14:06 +0100

    Implemented 5 UI tests

--- FitnessLogger/FitnessLoggerUITests/FitnessLoggerUITests.swift
+++ FitnessLogger/FitnessLoggerUITests/FitnessLoggerUITests.swift
@@ -9,12 +9,27 @@
 import XCTest
 
 class FitnessLoggerUITests: XCTestCase {
-
+    
+    var app = XCUIApplication()
+    
+    var isLaunched = false
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        
+//        app.launch()
+        
+//        print(isLaunched)
+        if !isLaunched {
+
+            print("Launching....")
+            app.launch()
+            isLaunched.toggle()
+            print(isLaunched)
+        }
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -22,61 +37,83 @@ class FitnessLoggerUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        let app2 = app
+    
+    func test1_SignIn() throws {
         
-        app2.textFields["Email"].clearText(andReplaceWith: "UITest@mail.com")
-        app2.secureTextFields["Password"].clearText(andReplaceWith: "UITestPassword")
+        app.textFields["Email"].clearText(andReplaceWith: "UITest@mail.com")
+        app.secureTextFields["Password"].clearText(andReplaceWith: "UITestPassword")
         
-        app2.buttons["Sign In"].tap()
+        app.buttons["Sign In"].tap()
         sleep(3)
-        app2.navigationBars["My Programs"].buttons["plus"].tap()
+
+        XCTAssertTrue(app.tabBars["Tab Bar"].buttons["3.circle"].isHittable)
+    }
+    
+    func test2_AddProgram() throws {
+
+        app.navigationBars["My Programs"].buttons["plus"].tap()
         sleep(1)
-        app2.textFields["Enter program name"].tap()
-        app2.textFields["Enter program name"].typeText("Something")
+        app.textFields["Enter program name"].tap()
+        app.textFields["Enter program name"].typeText("Something")
         sleep(1)
-        
-        app2.buttons["Add exercise"].tap()
+
+        app.buttons["Add exercise"].tap()
         sleep(1)
-        
-        app2.textFields["Enter exercise name"].tap()
-        app2.textFields["Enter exercise name"].typeText("Test Exercise")
+
+        app.textFields["Enter exercise name"].tap()
+        app.textFields["Enter exercise name"].typeText("Test Exercise")
         sleep(1)
-        
-        let accordianButtonsQuery = app2.buttons.matching(identifier: "Save")
+
+        let accordianButtonsQuery = app.buttons.matching(identifier: "Save")
         if accordianButtonsQuery.count > 0 {
             let firstButton = accordianButtonsQuery.element(boundBy: 1)
             firstButton.tap()
         }
         else {
-            app2.buttons["Save"].tap()
+            app.buttons["Save"].tap()
         }
 
         sleep(1)
-        app2.navigationBars["Add program"].buttons["Save"].tap()
+        app.navigationBars["Add program"].buttons["Save"].tap()
 
-        
-        app2.tabBars["Tab Bar"].buttons["3.circle"].tap()
-        app2.buttons["Sign Out"].tap()
-        
-        
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        XCTAssertTrue(app.tables.containing(.cell, identifier:"Something\n1 exercise").count == 1)
     }
     
-    
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    func test3_AddLog() throws {
+        app.tables.containing(.cell, identifier:"Something\n1 exercise").element.swipeDown()
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Something\n1 exercise"]/*[[".cells[\"Something\\n1 exercise\"].buttons[\"Something\\n1 exercise\"]",".buttons[\"Something\\n1 exercise\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app.tables.containing(.cell, identifier:"Test Exercise").element.swipeDown()
+        tablesQuery.buttons["Test Exercise"].tap()
+        
+        let plusButton = app.buttons["+"]
+        plusButton.tap()
+        plusButton.tap()
+        plusButton.tap()
+        plusButton.tap()
+        plusButton.tap()
+        app.buttons["Save"].tap()
+        app.navigationBars["Something"].buttons["My Programs"].tap()
     }
+    
+    func test4_DeleteProgram() throws {
+        app.tables.containing(.cell, identifier:"Something\n1 exercise").element.swipeDown()
+        app.tables/*@START_MENU_TOKEN@*/.buttons["Something\n1 exercise"]/*[[".cells[\"Something\\n1 exercise\"].buttons[\"Something\\n1 exercise\"]",".buttons[\"Something\\n1 exercise\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
+
+        app.tables.cells.element(boundBy: 0).buttons["Delete"].tap()
+        
+        XCTAssertTrue(app.buttons["Add training program"].isHittable)
+    }
+    
+    func test5_SignOut() throws {
+        
+        app.tabBars["Tab Bar"].buttons["3.circle"].tap()
+        app.buttons["Sign Out"].tap()
+
+        XCTAssertTrue(app.buttons["Sign In"].isHittable)
+    }
+
 }
 
 // Lån fra : https://stackoverflow.com/questions/32821880/ui-test-deleting-text-in-text-field
